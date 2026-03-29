13b2cd4b3929fa71ab414ffdcf145017296a5999
Muhammad <muhammadevvv@gmail.com>
2026-03-30 01:40:37 +0500

    m

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${specialElite.variable}  antialiased`}>
-        <header className="w-full p-2.5 flex justify-between">
+        <header className="w-full p-5 flex justify-between">
           <h1 className="text-sm">Khuranboyev Muhammad</h1>
           {/* <button className="text-xs">Inquiry</button> */}
         </header>
-        <main className="w-full h-[calc(100%-96px)]">{children}</main>
-        <footer className="p-2.5">
+        <main className="w-full h-[calc(100%-116px)]">{children}</main>
+        <footer className="p-5">
           <div className="text-[10px]"> Designing for a better harmonious world.</div>
           <div className="mt-1.5 flex justify-between">
             <div className="text-[10px] flex gap-2">
