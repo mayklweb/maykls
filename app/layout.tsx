c47805c3b1c9be756b715c861235093b0b3f4ed5
Muhammad <muhammadevvv@gmail.com>
2026-03-30 01:41:53 +0500

    m

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <h1 className="text-sm">Khuranboyev Muhammad</h1>
           {/* <button className="text-xs">Inquiry</button> */}
         </header>
-        <main className="w-full h-[calc(100%-116px)]">{children}</main>
+        <main className="w-full h-[calc(100%-126px)]">{children}</main>
         <footer className="p-5">
           <div className="text-[10px]"> Designing for a better harmonious world.</div>
           <div className="mt-1.5 flex justify-between">
